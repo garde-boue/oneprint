16bd3336fc876f4b53928600d66f3d8bde7aeb1e
Arthur Violy <arthur@violy.net>
2022-03-25 16:42:12 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = ()=> <footer className={"footer"}>
     <p>
-        <span><strong>One&nbsp;print&nbsp;a&nbsp;week • Une&nbsp;image&nbsp;par&nbsp;semaine</strong>{' © 2022 • '}</span>
+        <span><strong>One&nbsp;print&nbsp;a&nbsp;week</strong> • <strong>Une&nbsp;image&nbsp;par&nbsp;semaine</strong>{' '}•&nbsp;©&nbsp;2022&nbsp;•{' '}</span>
         <span></span>
         <span><a href={"https://anem.name"}>Anne‑Émilie&nbsp;Philippe</a> pour&nbsp;l’œuvre,</span>
     <br/><span><a href={"https://violy.net"}>Arthur&nbsp;Violy</a> pour le site internet,{' '}</span>
